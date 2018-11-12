60e38aeba0729315af565af7de26bd93bccc7628
Phat Huy <huy.le@kyanon.digital>
2018-11-12 17:38:11 +0700

    thrid commit

--- components/video/list/index.js
+++ components/video/list/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Link from 'next/link';
+const slugify = require('slugify');
 
 class VideoList extends React.Component {
   render() {
@@ -7,9 +8,14 @@ class VideoList extends React.Component {
   }
 
   renderVideoItems(item, index) {
+    // video/rich-kids-hoa-ra-cung-co-thu-ma-cac-con-nha-giau-khong-mua-duoc-5be7d8ba8040351ccda82392
+    let as =
+      'video/' +
+      slugify(item.sortTitle, { replacement: '-', lower: true }) +
+      '';
     return (
       <li key={item.id}>
-        <Link>
+        <Link as={as} href={href}>
           <a>{item.sortTitle}</a>
         </Link>
       </li>
